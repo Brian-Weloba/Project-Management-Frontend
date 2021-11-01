5e687354edad6780a3b28bf0abb378ec2a9ac068
Brian-Weloba <bweloba@gmail.com>
2021-11-01 08:31:30 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// const request = new XMLHttpRequest();
+// const url = 'https://tovutidevchallengeapi.azurewebsites.net/products';
+// request.open("GET", url);
+// request.send();
+
+// request.onload = (e) => {
+//     console.log(request.response)
+//     alert(request.response);
+// }
