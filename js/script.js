97e47ed7c76bf574edc0a554202d5cb3bec77be8
Brian-Weloba <bweloba@gmail.com>
2021-11-01 09:34:48 +0300

    add styling

--- js/script.js
+++ js/script.js
@@ -7,3 +7,10 @@
 //     console.log(request.response)
 //     alert(request.response);
 // }
+
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+searchButton.addEventListener('click', () => {
+    const inputValue = searchInput.value;
+    alert(inputValue);
+});
